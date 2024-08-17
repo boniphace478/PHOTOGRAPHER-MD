e4699c9c14b87bac5a2a587bc4eed2b5bbb31ec6
BoniphaceTeach <176963808+boniphace478@users.noreply.github.com>
2024-08-17 04:30:34 +0300

    Update yt-search.js

--- commandes/yt-search.js
+++ commandes/yt-search.js
@@ -19,9 +19,9 @@ zokou({ nomCom: "yts", categorie: "Search", reaction: "💎" }, async (dest, zk,
 
     let captions = "";
 for (let i = 0; i < 15; i++) {
-  captions += `╭━━LUCKY MD V5 YOUTUBE MENU━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
+  captions += `╭━━ℙℍ𝕆𝕋𝕆𝔾ℝ𝔸ℙℍ𝔼ℝ 𝕄𝔻 YOUTUBE MENU━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
 }
-    captions += "\n======\n*powered by Fredie Tech*";
+    captions += "\n======\n*powered by Photographer Tech*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
@@ -77,7 +77,7 @@ _*En cours de téléchargement...*_\n\n`
 
     fileStream.on('finish', () => {
       // Envoi du fichier vidéo en utilisant l'URL du fichier local
-      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *Zokou-Md*", gifPlayback: false }, { quoted: ms });
+      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *ℙℍ𝕆𝕋𝕆𝔾ℝ𝔸ℙℍ𝔼ℝ 𝕄𝔻*", gifPlayback: false }, { quoted: ms });
 
     });
 
