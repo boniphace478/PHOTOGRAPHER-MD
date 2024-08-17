4f10bb48c99994eefbbc30c767aeb61d6fcc00ec
BoniphaceTeach <176963808+boniphace478@users.noreply.github.com>
2024-08-17 04:22:05 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_uptime of lucky md v5 bot is: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_uptime of ℙℍ𝕆𝕋𝕆𝔾ℝ𝔸ℙℍ𝔼ℝ 𝕄𝔻 bot is: ${runtime(process.uptime())}_*`) 
 
    
 
